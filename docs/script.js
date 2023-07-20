4bf1d71044fe927bb78e66f71ed608fc1f8aedfd
MYTE <yasmi.tohabar@gmail.com>
2023-07-20 21:31:24 +0600

    comment unnecessary codes

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,29 @@
+async function loaded(reader) {
+    try {
+        const response = await fetch("https://myte-ic-photography-styles.hf.space/run/predict", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                data: [reader.result]
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error("Error: HTTP status " + response.status);
+        }
+
+        const json = await response.json();
+        const label = json.data[0].label;
+
+        results.innerHTML = `<p>${label}</p>
+                                     <p>${json.data[0].confidences[0].confidence}</p>`;
+    } catch (error) {
+        console.error("An error occurred: ", error);
+    }
+}
+
 function readURL(input) {
     let image_input = $(input);
     if (input.files && input.files[0]) {
@@ -13,6 +39,7 @@ function readURL(input) {
             image_input.closest('.file-upload').find('.image-title').html(input.files[0].name);
         };
 
+        reader.addEventListener("load", () => loaded(reader))
         reader.readAsDataURL(input.files[0]);
 
     } else {
